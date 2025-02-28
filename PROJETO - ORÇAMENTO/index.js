66f88a49a6d6198f627a53eb7c0fc6cc31d1950b
Felipe130915 <feliphi10@gmail.com>
2025-02-27 23:39:36 -0300

    Add files via upload

---
+++ PROJETO - ORÇAMENTO/index.js
@@ -0,0 +1,374 @@
+
+function Soma1(){  /* FUNÇÃO QUE FAZ A MULTIPLICAÇÃO DOS INPUS DA QUANTIDADE E DO VALOR INITARIO DOS PRODUTOS*/
+    var soma = 0;
+    var ipts = document.querySelectorAll('input[oninput="Soma1()"]');
+    for(var x=0; x<ipts.length; x++){
+       var valorItem = parseFloat(ipts[x].value);
+       if(soma ===0)
+          soma = valorItem;
+       else{
+         !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+         console.log(soma);
+       }
+    }
+    document.querySelector('#Total1').value = soma.toFixed(2);
+ }
+
+ function Soma2(){
+    var soma = 0;
+    var ipts = document.querySelectorAll('input[oninput="Soma2()"]');
+    for(var x=0; x<ipts.length; x++){
+       var valorItem = parseFloat(ipts[x].value);
+       if(soma ===0)
+          soma = valorItem;
+       else{
+         !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+         console.log(soma);
+       }
+    }
+    document.querySelector('#Total2').value = soma.toFixed(2);
+ }
+ 
+ function Soma3(){
+    var soma = 0;
+    var ipts = document.querySelectorAll('input[oninput="Soma3()"]');
+    for(var x=0; x<ipts.length; x++){
+       var valorItem = parseFloat(ipts[x].value);
+       if(soma ===0)
+          soma = valorItem;
+       else{
+         !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+         console.log(soma);
+       }
+    }
+    document.querySelector('#Total3').value = soma.toFixed(2);
+ }
+
+ function Soma4(){
+    var soma = 0;
+    var ipts = document.querySelectorAll('input[oninput="Soma4()"]');
+    for(var x=0; x<ipts.length; x++){
+       var valorItem = parseFloat(ipts[x].value);
+       if(soma ===0)
+          soma = valorItem;
+       else{
+         !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+         console.log(soma);
+       }
+    }
+    document.querySelector('#Total4').value = soma.toFixed(2);
+ }
+
+ function Soma5(){
+    var soma = 0;
+    var ipts = document.querySelectorAll('input[oninput="Soma5()"]');
+    for(var x=0; x<ipts.length; x++){
+       var valorItem = parseFloat(ipts[x].value);
+       if(soma ===0)
+          soma = valorItem;
+       else{
+         !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+         console.log(soma);
+       }
+    }
+    document.querySelector('#Total5').value = soma.toFixed(2);
+ }
+
+ function Soma6(){
+    var soma = 0;
+    var ipts = document.querySelectorAll('input[oninput="Soma6()"]');
+    for(var x=0; x<ipts.length; x++){
+       var valorItem = parseFloat(ipts[x].value);
+       if(soma ===0)
+          soma = valorItem;
+       else{
+         !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+         console.log(soma);
+       }
+    }
+    document.querySelector('#Total6').value = soma.toFixed(2);
+ }
+
+ function Soma7(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma7()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total7').value = soma.toFixed(2);
+}
+
+function Soma8(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma8()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total8').value = soma.toFixed(2);
+}
+
+function Soma9(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma9()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total9').value = soma.toFixed(2);
+}
+
+function Soma10(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma10()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total10').value = soma.toFixed(2);
+}
+
+function Soma11(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma11()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total11').value = soma.toFixed(2);
+}
+
+function Soma12(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma12()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total12').value = soma.toFixed(2);
+}
+
+function Soma13(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma13()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total13').value = soma.toFixed(2);
+}
+
+function Soma14(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma14()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total14').value = soma.toFixed(2);
+}
+
+function Soma15(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma15()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total15').value = soma.toFixed(2);
+}
+
+function Soma16(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma16()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total16').value = soma.toFixed(2);
+}
+
+function Soma17(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma17()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total17').value = soma.toFixed(2);
+}
+
+function Soma18(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma18()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total18').value = soma.toFixed(2);
+}
+
+function Soma19(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma19()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total19').value = soma.toFixed(2);
+}
+
+function Soma20(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma20()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total20').value = soma.toFixed(2);
+}
+
+function Soma21(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma21()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total21').value = soma.toFixed(2);
+}
+
+function Soma22(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma22()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total22').value = soma.toFixed(2);
+}
+
+function Soma23(){
+   var soma = 0;
+   var ipts = document.querySelectorAll('input[oninput="Soma23()"]');
+   for(var x=0; x<ipts.length; x++){
+      var valorItem = parseFloat(ipts[x].value);
+      if(soma ===0)
+         soma = valorItem;
+      else{
+        !isNaN(valorItem) ? soma *= parseFloat(valorItem) : null;
+        console.log(soma);
+      }
+   }
+   document.querySelector('#Total23').value = soma.toFixed(2);
+}
+
+/* aeeee formula que deu certo J.s se for preciso é so mudar o parserint para paserfloat para receber numeros quebrados ou seja recerber string */
+
+function clicar() {
+   var Total1 = document.querySelector(".Total1").value;  /* total1 Representa a class do input*/
+   var Total2 = document.querySelector(".Total2").value; /* total2 Representa a class do input*/
+
+// caculo do desconto
+   var Recebedesconto = document.querySelector(".Desconto").value; /* Recebe o valor do desconto*/
+   var Dividedesconto = 100;
+   var produtosdesconto = parseFloat(Total1.replace(',', '.')) + parseFloat(Total2.replace(',', '.'));  // replace(',', '.') usado para os inputs aceitarem numeros co virgula
+   var Transformaemporcentagem = Recebedesconto / Dividedesconto;
+   var Desconto = produtosdesconto * Transformaemporcentagem;
+ // calculo do desconto
+
+   var Total_produtos = parseFloat(Total1.replace(',', '.')) + parseFloat(Total2.replace(',', '.')) - Desconto;
+   var comissao = 0.10; /*var que calcula a comissão de 10%*/
+   var Servico = Total_produtos * comissao;
+
+   
+   var resultado = Total_produtos + Servico;
+   document.querySelector(".resultado").value = resultado. toFixed(2); /* resultado Representa a class do input "importante para o valor do total ficar dentro de um input e necessario usar   .value = resultado. toFixed(2); */
+
+}
+
+
+
+
+
